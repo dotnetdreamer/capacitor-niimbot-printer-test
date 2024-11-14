a552027f62ba8bf95ddeab28c2414c8dcbe66d4e
idrees.khan1 <idrees.khan1@thermofisher.com>
2024-11-14 14:37:31 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-niimbot-printer-test',
+  webDir: 'www'
+};
+
+export default config;
